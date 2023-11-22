6374235e46316f9c3f562cfa3f0a47c928d50473
Matheus0010101 <133948068+Matheus0010101@users.noreply.github.com>
2023-11-22 16:37:03 -0300

    Commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// atividade 1
+
+let nome = prompt('digite seu nome')
+let cidade= prompt('digite sua cidade')
+
+console.log (nome + cidade)
+
+// atividade 2:
+let primeiraPergunta = prompt("Você gosta de basquete?") 
+console.log("Você gosta de basquete?", primeiraPergunta)
+
+let segundaPergunta = prompt("Voce gosta de programar?") 
+console.log("Voce gosta de programar?", segundaPergunta)
+
+let terceiraPergunta = prompt("voce trabalha?") 
+console.log("voce trabalha?", terceiraPergunta)
+
+// atividade 3
+
+let x = 5;
+let y = 10;
+let z ;
+
+
+z=x 
+x=y
+y=z
+
+console.log("Troquei valores x =10", x)
+console.log("Troquei valores y =5", y)
+
+// Atividade 4
+
+let primeiroNumero = Number(prompt("insira o primeiro numero"))
+let segundoNumero = Number(prompt("insira o segundo numero"))
+
+let somar = primeiroNumero + segundoNumero
+console.log(somar)
+w
+let multiplicar = primeiroNumero * segundoNumero
+
+
